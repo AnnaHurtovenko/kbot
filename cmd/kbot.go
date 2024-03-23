506e65fd3646562937d180c8796551f7649ccdde
AnnaHurtovenko <163589983+AnnaHurtovenko@users.noreply.github.com>
2024-03-23 17:56:28 +0000

    create telebot

---
+++ cmd/kbot.go
@@ -0,0 +1,77 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	//Teletoken bot
+	TeleToken = os.Getenv("TELE_TOKEN")
+)
+
+// kbotCmd represents the kbot command
+var kbotCmd = &cobra.Command{
+	Use:   "kbot",
+	Aliases: []string{"start"},
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		fmt.Printf("kbot %s called", appVersion)
+
+		kbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please check TeleToken env variable. %s", err)
+			return
+		}
+
+		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+
+			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+				case "hello":
+					err = m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
+			}
+
+			return err
+
+		})
+		kbot.Start()
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(kbotCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// kbotCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// kbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
